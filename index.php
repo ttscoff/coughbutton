d0adf92498483aea041ae234b221e7a2da040a5b
Brett Terpstra <me@brettterpstra.com>
2012-10-21 09:24:13 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL|E_STRICT);
+if (isset($_REQUEST['func']) && $_REQUEST['func'] == 'mutemymic')
+{
+	// Triggers opt-F5 to toggle MuteMyMic
+	// https://itunes.apple.com/pl/app/mutemymic/id456362093?mt=12&partnerId=30&siteID=vRL5rYo4h5A
+	$ret = exec("osascript -e 'tell application \"System Events\" to key code 96 using option down'");
+	die($ret);
+} ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<title>CoughButton</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link rel="stylesheet" href="styles/cough.css">
+</head>
+<body>
+
+	<a href="#" class="btn2" id="cough">COUGH!</a>
+
+	<script type="text/javascript" src="scripts/jquery18.js" ></script>
+	<script type="text/javascript" src="scripts/hammer.js"></script>
+	<script type="text/javascript" src="scripts/jquery.hammer.js"></script>
+
+	<script>
+		(function($){
+			setTimeout(function(){window.scrollTo(0,27);},500);
+			$("#cough").hammer({
+					prevent_default: true,
+					hold_timeout: 0
+				}).bind("hold", function(ev) {
+					$.get("index.php?func=mutemymic");
+					$("body,html").css({backgroundColor:"red"});
+					return false;
+				}).bind("release", function(ev) {
+					$.get("index.php?func=mutemymic");
+					$("body,html").css({background:"#478b35"});
+					return false;
+				});
+		})(jQuery);
+	</script>
+</body>
+</html>
---
+++ styles/README.md
@@ -0,0 +1,25 @@
+# CoughButton
+
+A little ditty by Brett Terpstra
+
+## Purpose
+
+CoughButton is a small PHP script and iPhone-friendly web app that does one simple thing: press opt-F5 on your Mac to toggle MuteMyMic[1].
+
+[1]: https://itunes.apple.com/pl/app/mutemymic/id456362093?mt=12
+
+CoughButton uses jQuery and the [Hammer.js][2] jQuery plugin to provide a tap-and-hold feature so that your iPhone functions like a real cough button. Press and hold the screen and your mic will be muted, when you let up it will unmute.
+
+## Installation
+
+You need a local web server running PHP and a way to load it on your iPhone. This can be done by turning on the apache web server with PHP enabled and setting up a virtual host. If you don't know how to do that, I recommend using [MAMP][3].
+
+[3]: http://www.mamp.info/en/index.html
+
+You'll also need [MuteMyMic][1] installed and running.
+
+## Usage
+
+Open your virtual host address/port on your iPhone. Use the send button at the bottom of the mobile Safari window to "Add to Homescreen." Leave Safari and launch the new icon it creates. Make sure that MuteMyMic is running and set to unmuted. Tap and hold your screen and you should see the MuteMyMic icon in your menu bar turn red, returning to black when you release.
+
+If you're barefoot and don't mind touching your iPhone with your feet, it makes a decent foot pedal, too.
